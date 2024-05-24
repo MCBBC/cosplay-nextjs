5105f9334ced24d43677d25c32dec94ab0203102
HideInMatrix <davidmorgancreate@gmail.com>
2024-05-24 16:02:42 +0800

    feat:登陆重定向登陆页面

--- auth.config.ts
+++ auth.config.ts
@@ -8,11 +8,16 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      const callbackUrl = nextUrl.searchParams.get("callbackUrl");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
+        if(callbackUrl){
+          return Response.redirect(new URL(callbackUrl, nextUrl));
+        }else{
+          return false;
+        }
       }
       return true;
     },
