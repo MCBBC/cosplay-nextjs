643c928bd6d4296a8ac92de42166dfe949675a2a
HideInMatrix <davimorgancreate@gmail.com>
2024-05-31 17:03:07 +0800

    feat:Coser搜索功能对接

--- app/front/cosers/page.tsx
+++ app/front/cosers/page.tsx
@@ -1,4 +1,11 @@
 import { CosersMain } from "@/app/ui/cosers/cosers";
-export default function Page() {
-  return <CosersMain></CosersMain>;
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  return <CosersMain searchParams={searchParams}></CosersMain>;
 }
