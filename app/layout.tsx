b73920c3ef0a9c6503f4c4f5c807474c2578c729
HideInMatrix <davidmorgancreate@gmail.com>
2024-06-23 19:20:40 +0800

    feat:关闭广告检测

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import NprogressProvider from "@/components/NProgressProvider";
 import AdSense from "@/components/AdSense";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
-import { BlockAdBlock } from "@/app/ui/common/block-ad-block";
+// import { BlockAdBlock } from "@/app/ui/common/block-ad-block";
 
 export const metadata: Metadata = {
   title: {
@@ -35,7 +35,7 @@ export default function RootLayout({
           {children}
           {/* </Providers> */}
         </NprogressProvider>
-        <BlockAdBlock />
+        {/* <BlockAdBlock /> */}
       </body>
     </html>
   );
