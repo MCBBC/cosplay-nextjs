1ecf9a1a86eaf5ee8a8cbe6aa45ee1e63b45c526
HideInMatrix <davidmorgancreate@gmail.com>
2024-08-11 12:13:58 +0800

    fix:更新广告拦截

--- app/ui/common/block-ad-block.tsx
+++ app/ui/common/block-ad-block.tsx
@@ -1,20 +1,13 @@
 "use client";
-import { useEffect } from "react";
 import { useRouter } from "next-nprogress-bar";
 import { useDetectAdBlock } from "adblock-detect-react";
-import { usePathname } from "next/navigation";
 
 export function BlockAdBlock() {
-  const adBlockDetected = useDetectAdBlock();
   const router = useRouter();
-  const pathname = usePathname();
+  const adBlockDetected = useDetectAdBlock();
 
-  useEffect(() => {
-    // console.log("router change", adBlockDetected);
-
-    if (adBlockDetected) {
-      router.push("/adBlock");
-    }
-  }, [pathname, adBlockDetected]);
+  if (adBlockDetected) {
+    router.push("/adBlock");
+  }
   return <></>;
 }
