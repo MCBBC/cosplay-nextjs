c803bf84ec1c52676ef3df670821a73183a560df
HideInMatrix <davidmorgancreate@gmail.com>
2024-08-11 12:28:21 +0800

    fix:更新广告拦截

--- app/ui/common/auto-google.tsx
+++ app/ui/common/auto-google.tsx
@@ -2,7 +2,6 @@
 import { usePathname } from "next/navigation";
 import AdSense from "@/components/AdSense";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { BlockAdBlock } from "@/app/ui/common/block-ad-block";
 export function AutoGoogle() {
   const pathName = usePathname();
   const isDashboard = pathName.startsWith("/dashboard");
@@ -13,7 +12,6 @@ export function AutoGoogle() {
         <>
           <AdSense pId="5901616898778649" />
           <GoogleAnalytics gaId="G-15MZJRZZB1" />
-          <BlockAdBlock />
         </>
       )}
     </>
--- app/ui/common/block-ad-block.tsx
+++ app/ui/common/block-ad-block.tsx
@@ -1,13 +1,19 @@
 "use client";
+import { useEffect } from "react";
 import { useRouter } from "next-nprogress-bar";
 import { useDetectAdBlock } from "adblock-detect-react";
+import { usePathname } from "next/navigation";
 
 export function BlockAdBlock() {
   const router = useRouter();
+  const pathname = usePathname();
   const adBlockDetected = useDetectAdBlock();
+  useEffect(() => {
+    // console.log("router change", adBlockDetected);
 
-  if (adBlockDetected) {
-    router.push("/adBlock");
-  }
+    if (adBlockDetected) {
+      router.push("/adBlock");
+    }
+  }, [pathname]);
   return <></>;
 }
